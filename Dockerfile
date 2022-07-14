2543d8d07aaf0b9af4000a7ff797bb4a2e96fd70
Saurabh Chauhan <saurabh.c@nopaperforms.com>
2022-07-14 18:23:48 +0530

    prereqs.sh-Added

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM ubuntu:20.04
 RUN apt-get update && apt-get install -y tzdata && apt install -y python3.8 python3-pip
 RUN apt install python3-dev libpq-dev nginx python3-pip python3-virtualenv sqlitebrowser wget ruby-full -y
-RUN wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-CMD ["chmod", "+x", "./install"]
-CMD ["sudo", "./install", "auto"]
+#RUN wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+#CMD ["chmod", "+x", "./install"]
+#CMD ["sudo", "./install", "auto"]
 #RUN pip install django gunicorn psycopg2
 ADD . /opt/django-helloworld/
 WORKDIR /opt/django-helloworld/
---
+++ prereqs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This script will install EKS prerequisites on Amazon Linux or Amazon Linux 2
+# * eksctl
+# * AWS CLI
+
+set -e
+
+mkdir -p $HOME/bin
+echo 'export PATH=$HOME/bin:$PATH' >>~/.bashrc
+
+# AWS CLI
+if ! type aws >/dev/null 2>&1; then
+	curl -o awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+	unzip awscli-bundle.zip
+	./awscli-bundle/install -b $HOME/bin/aws
+	echo 'AWS CLI installed'
+else
+	echo 'AWS CLI already installed'
+fi
+
+# eksctl
+if ! type eksctl >/dev/null 2>&1; then
+	curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+	mv /tmp/eksctl $HOME/bin
+	echo 'eksctl installed'
+else
+	echo 'eksctl already installed'
+fi
+# Test if AWS credentials exist
+aws sts get-caller-identity
