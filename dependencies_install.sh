867a4a1bcc55826a9122d64e24c9d4b8f1583396
Saurabh Chauhan <saurabh.c@nopaperforms.com>
2022-07-18 16:09:21 +0530

    Updated_18_July

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
@@ -18,4 +18,4 @@ echo region = us-east-1 >> /root/.aws/config
 echo output = json >> /root/.aws/config
 #      - chmod +x create_iam_role.sh
 #      - sh create_iam_role.sh
-aws eks update-kubeconfig --name eks-django-app --region us-east-1
+#aws eks update-kubeconfig --name eks-django-app --region us-east-1
