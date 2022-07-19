29143a6c574b616adb8830d364829e53def47851
Saurabh Chauhan <saurabh.c@nopaperforms.com>
2022-07-19 13:32:42 +0530

    django-project-1.03

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Deployment with Pipeline successfully Completed")
+    return HttpResponse("Deployment with Pipeline successfully Completed 1-5")
