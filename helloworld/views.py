12e59e7113fe39f3c393e05a6e0c1df29bab4bbd
Saurabh Chauhan <saurabh.c@nopaperforms.com>
2022-07-25 15:58:44 +0530

    Ingress-Service-Added-in-EKS

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Deployment with Pipeline successfully Completed 35")
+    return HttpResponse("Deployment with Pipeline successfully Completed with EKS ALB Ingress Service")
