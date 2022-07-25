856b02550b5ccd53e9c45b716e71f7034a5b3ce0
Saurabh Chauhan <saurabh.c@nopaperforms.com>
2022-07-25 19:30:04 +0530

    Rollback-Test

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Deployment with Pipeline successfully Completed with EKS ALB Ingress Service")
+    return HttpResponse("Deployment with Pipeline successfully Completed with EKS ALB Ingress Service Rollback Deloyments Test")
